6e262c5084367fb6b5ee5b493376907e7df4889c
priyadharshini517 <priyadharshinirameshr2006@gmail.com>
2026-04-13 20:55:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-document.getElementById("feedbackForm").addEventListener("submit", function(e) {
+const form = document.getElementById("feedbackForm");
+
+form.addEventListener("submit", function(e) {
   e.preventDefault();
 
-  alert("Clicked 🔥");   // 👈 test
+  const name = document.getElementById("name").value;
+  const department = document.getElementById("department").value;
+  const category = document.getElementById("category").value;
+  const feedback = document.getElementById("feedback").value;
 
-  const data = {
-    name: document.getElementById("name").value
-  };
-
-  db.ref("feedbacks").push(data)
-    .then(() => {
-      alert("Saved 🔥");
+  fetch("https://script.google.com/macros/s/AKfycbxaIkTGpp3AL3vXMtTUbXO1LPFIg24dbwSueaOHAa4robVvNWoDdZFGkW326ITZbdRP/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      name: name,
+      department: department,
+      category: category,
+      feedback: feedback
     })
-    .catch(() => {
-      alert("Error ❌");
-    });
+  })
+  .then(res => res.text())
+  .then(data => {
+    alert("Feedback submitted successfully!");
+    form.reset();
+  })
+  .catch(err => {
+    alert("Error ❌");
+    console.log(err);
+  });
 });
-
